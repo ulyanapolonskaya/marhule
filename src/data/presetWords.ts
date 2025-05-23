7e1cd7a322e9fb016b029285e82608944b0d3216
ulyanapolonskaya <ulyana.polonskaya@gmail.com>
2025-05-23 23:03:15 +0200

    added words

--- src/data/presetWords.ts
+++ src/data/presetWords.ts
@@ -65,5 +65,611 @@ export const presetWords: WordPair[] = [
     slovak: "dobre",
     learned: false,
     id: "10"
+  },
+  {
+    russian: "тот, та, то",
+    slovak: "ten, tá, to",
+    learned: false,
+    id: "11"
+  },
+  {
+    russian: "этот, эта, это",
+    slovak: "tento, táto, toto",
+    learned: false,
+    id: "12"
+  },
+  {
+    russian: "какой-нибудь, какая-нибудь, какое-нибудь; какой-то, какая-то, какое-то",
+    slovak: "nejaký, nejaká, nejaké",
+    learned: false,
+    id: "13"
+  },
+  {
+    russian: "там",
+    slovak: "tam",
+    learned: false,
+    id: "14"
+  },
+  {
+    russian: "здесь",
+    slovak: "tu",
+    learned: false,
+    id: "15"
+  },
+  {
+    russian: "объекты, находящиеся поблизости",
+    slovak: "tento, táto, toto",
+    learned: false,
+    id: "16"
+  },
+  {
+    russian: "отдаленные предметы",
+    slovak: "tamten, tamtá, tamto",
+    learned: false,
+    id: "17"
+  },
+  {
+    russian: "общежитие",
+    slovak: "internát",
+    learned: false,
+    id: "18"
+  },
+  {
+    russian: "письмо",
+    slovak: "list",
+    learned: false,
+    id: "19"
+  },
+  {
+    russian: "жить, поживать, чувствовать себя",
+    slovak: "mať sa",
+    learned: false,
+    id: "20"
+  },
+  {
+    russian: "спокойной ночи",
+    slovak: "dobrú noc",
+    learned: false,
+    id: "21"
+  },
+  {
+    russian: "прощай(те)",
+    slovak: "zbohom",
+    learned: false,
+    id: "22"
+  },
+  {
+    russian: "Очень приятно.",
+    slovak: "Teší ma.",
+    learned: false,
+    id: "23"
+  },
+  {
+    russian: "плохо",
+    slovak: "zle",
+    learned: false,
+    id: "24"
+  },
+  {
+    russian: "также, тоже",
+    slovak: "tiež, aj",
+    learned: false,
+    id: "25"
+  },
+  {
+    russian: "жить",
+    slovak: "bývat'",
+    learned: false,
+    id: "26"
+  },
+  {
+    russian: "семья",
+    slovak: "rodina",
+    learned: false,
+    id: "27"
+  },
+  {
+    russian: "братья и сестры",
+    slovak: "súrodenci",
+    learned: false,
+    id: "28"
+  },
+  {
+    russian: "внуки",
+    slovak: "vnúčatá",
+    learned: false,
+    id: "29"
+  },
+  {
+    russian: "некрасивый",
+    slovak: "škaredý",
+    learned: false,
+    id: "30"
+  },
+  {
+    russian: "милый",
+    slovak: "milý",
+    learned: false,
+    id: "31"
+  },
+  {
+    russian: "её",
+    slovak: "jej",
+    learned: false,
+    id: "32"
+  },
+  {
+    russian: "уже",
+    slovak: "už",
+    learned: false,
+    id: "33"
+  },
+  {
+    russian: "еще",
+    slovak: "ešte",
+    learned: false,
+    id: "34"
+  },
+  {
+    russian: "юг",
+    slovak: "juh",
+    learned: false,
+    id: "35"
+  },
+  {
+    russian: "лошадь, конь",
+    slovak: "kôň",
+    learned: false,
+    id: "36"
+  },
+  {
+    russian: "север",
+    slovak: "sever",
+    learned: false,
+    id: "37"
+  },
+  {
+    russian: "Вена - в Вену",
+    slovak: "Viedeň - do Viedne",
+    learned: false,
+    id: "38"
+  },
+  {
+    russian: "восток",
+    slovak: "východ",
+    learned: false,
+    id: "39"
+  },
+  {
+    russian: "запад",
+    slovak: "západ",
+    learned: false,
+    id: "40"
+  },
+  {
+    russian: "обменный пункт",
+    slovak: "zmenáreň",
+    learned: false,
+    id: "41"
+  },
+  {
+    russian: "ученик",
+    slovak: "žiak",
+    learned: false,
+    id: "42"
+  },
+  {
+    russian: "в одну сторону / обратный",
+    slovak: "jednosmerný / spiatočný",
+    learned: false,
+    id: "43"
+  },
+  {
+    russian: "прошедший",
+    slovak: "minulý",
+    learned: false,
+    id: "44"
+  },
+  {
+    russian: "желать",
+    slovak: "priať si",
+    learned: false,
+    id: "45"
+  },
+  {
+    russian: "просить",
+    slovak: "prosiť si",
+    learned: false,
+    id: "46"
+  },
+  {
+    russian: "стоить",
+    slovak: "stáť (cena)",
+    learned: false,
+    id: "47"
+  },
+  {
+    russian: "куда",
+    slovak: "kam",
+    learned: false,
+    id: "48"
+  },
+  {
+    russian: "никакой",
+    slovak: "žiadny",
+    learned: false,
+    id: "49"
+  },
+  {
+    russian: "сначала",
+    slovak: "najskôr",
+    learned: false,
+    id: "50"
+  },
+  {
+    russian: "мотоцикл",
+    slovak: "motorka",
+    learned: false,
+    id: "51"
+  },
+  {
+    russian: "пешком",
+    slovak: "pešo",
+    learned: false,
+    id: "52"
+  },
+  {
+    russian: "обратно",
+    slovak: "späť",
+    learned: false,
+    id: "53"
+  },
+  {
+    russian: "налево",
+    slovak: "vľavo",
+    learned: false,
+    id: "54"
+  },
+  {
+    russian: "сзади",
+    slovak: "vzadu",
+    learned: false,
+    id: "55"
+  },
+  {
+    russian: "только",
+    slovak: "len",
+    learned: false,
+    id: "56"
+  },
+  {
+    russian: "Сколько это стоит?",
+    slovak: "Koľko to stojí?",
+    learned: false,
+    id: "57"
+  },
+  {
+    russian: "Пожалуйста (вот).",
+    slovak: "Nech sa páči.",
+    learned: false,
+    id: "58"
+  },
+  {
+    russian: "лекция",
+    slovak: "prednáška",
+    learned: false,
+    id: "59"
+  },
+  {
+    russian: "перемена, перерыв - на перемене, во время перерыва",
+    slovak: "prestávka - cez prestávku",
+    learned: false,
+    id: "60"
+  },
+  {
+    russian: "одноклассник",
+    slovak: "spolužiak",
+    learned: false,
+    id: "61"
+  },
+  {
+    russian: "туризм",
+    slovak: "turistika",
+    learned: false,
+    id: "62"
+  },
+  {
+    russian: "любить",
+    slovak: "mať rád",
+    learned: false,
+    id: "63"
+  },
+  {
+    russian: "покупать (делать покупки)",
+    slovak: "nakupovať",
+    learned: false,
+    id: "64"
+  },
+  {
+    russian: "садиться",
+    slovak: "nastupovať",
+    learned: false,
+    id: "65"
+  },
+  {
+    russian: "чистить зубы",
+    slovak: "umyť si zuby",
+    learned: false,
+    id: "66"
+  },
+  {
+    russian: "знать",
+    slovak: "vedieť",
+    learned: false,
+    id: "67"
+  },
+  {
+    russian: "веселиться; развлекаться",
+    slovak: "zabávať sa",
+    learned: false,
+    id: "68"
+  },
+  {
+    russian: "Когда ты утром встаешь?",
+    slovak: "Kedy ráno vstávaš?",
+    learned: false,
+    id: "69"
+  },
+  {
+    russian: "весной",
+    slovak: "na jar",
+    learned: false,
+    id: "70"
+  },
+  {
+    russian: "летом",
+    slovak: "v lete",
+    learned: false,
+    id: "71"
+  },
+  {
+    russian: "загорать",
+    slovak: "opaľovať sa",
+    learned: false,
+    id: "72"
+  },
+  {
+    russian: "плавать",
+    slovak: "kúpať",
+    learned: false,
+    id: "73"
+  },
+  {
+    russian: "осенью",
+    slovak: "na jeseň",
+    learned: false,
+    id: "74"
+  },
+  {
+    russian: "зимой",
+    slovak: "v zime",
+    learned: false,
+    id: "75"
+  },
+  {
+    russian: "купальник",
+    slovak: "plavky",
+    learned: false,
+    id: "76"
+  },
+  {
+    russian: "рубашка",
+    slovak: "košeľa",
+    learned: false,
+    id: "77"
+  },
+  {
+    russian: "картина, рисунок, иллюстрация",
+    slovak: "obraz, obrázok",
+    learned: false,
+    id: "78"
+  },
+  {
+    russian: "пижама",
+    slovak: "pyžamo",
+    learned: false,
+    id: "79"
+  },
+  {
+    russian: "джинсы",
+    slovak: "rifle",
+    learned: false,
+    id: "80"
+  },
+  {
+    russian: "рукавицы",
+    slovak: "rukavice",
+    learned: false,
+    id: "81"
+  },
+  {
+    russian: "пиджак",
+    slovak: "sako",
+    learned: false,
+    id: "82"
+  },
+  {
+    russian: "юбка",
+    slovak: "sukňa",
+    learned: false,
+    id: "83"
+  },
+  {
+    russian: "сумочка",
+    slovak: "kabelka",
+    learned: false,
+    id: "84"
+  },
+  {
+    russian: "одинаковый",
+    slovak: "rovnaký",
+    learned: false,
+    id: "85"
+  },
+  {
+    russian: "поздравлять",
+    slovak: "blahoželať",
+    learned: false,
+    id: "86"
+  },
+  {
+    russian: "одеваться",
+    slovak: "obliekať si",
+    learned: false,
+    id: "87"
+  },
+  {
+    russian: "обуваться",
+    slovak: "obúvať si",
+    learned: false,
+    id: "88"
+  },
+  {
+    russian: "нравиться",
+    slovak: "páčiť sa",
+    learned: false,
+    id: "89"
+  },
+  {
+    russian: "выбрать (себе)",
+    slovak: "vybrať si",
+    learned: false,
+    id: "90"
+  },
+  {
+    russian: "примерить; испытать",
+    slovak: "vyskúšať si",
+    learned: false,
+    id: "91"
+  },
+  {
+    russian: "примерочная кабинка",
+    slovak: "skúšobná kabínka",
+    learned: false,
+    id: "92"
+  },
+  {
+    russian: "Я могу это примерить?",
+    slovak: "Môžem si to vyskúšať?",
+    learned: false,
+    id: "93"
+  },
+  {
+    russian: "Это мне (не) нравится.",
+    slovak: "To sa mi (ne)páči.",
+    learned: false,
+    id: "94"
+  },
+  {
+    russian: "одежда",
+    slovak: "mám oblečený sveter / mám oblečenú blúzku / mám oblečené tričko (oblečenie)",
+    learned: false,
+    id: "95"
+  },
+  {
+    russian: "обувь",
+    slovak: "mám obuté čižmy / tenisky / lodičky (obuv)",
+    learned: false,
+    id: "96"
+  },
+  {
+    russian: "аксессуары",
+    slovak: "mám rukavice / čiapku / kabelku (doplnky)",
+    learned: false,
+    id: "97"
+  },
+  {
+    russian: "ручка",
+    slovak: "pero",
+    learned: false,
+    id: "98"
+  },
+  {
+    russian: "работа (трудоустройство)",
+    slovak: "práca (zamestnanie)",
+    learned: false,
+    id: "99"
+  },
+  {
+    russian: "расписание",
+    slovak: "rozvrh hodín",
+    learned: false,
+    id: "100"
+  },
+  {
+    russian: "считать",
+    slovak: "počítať",
+    learned: false,
+    id: "101"
+  },
+  {
+    russian: "спешить, торопиться",
+    slovak: "ponáhľať sa",
+    learned: false,
+    id: "102"
+  },
+  {
+    russian: "стать, сделаться",
+    slovak: "stať sa",
+    learned: false,
+    id: "103"
+  },
+  {
+    russian: "длиться, продолжаться",
+    slovak: "trvať",
+    learned: false,
+    id: "104"
+  },
+  {
+    russian: "куда",
+    slovak: "kam",
+    learned: false,
+    id: "105"
+  },
+  {
+    russian: "после обеда",
+    slovak: "popoludní",
+    learned: false,
+    id: "106"
+  },
+  {
+    russian: "лучше",
+    slovak: "radšej",
+    learned: false,
+    id: "107"
+  },
+  {
+    russian: "приблизительно; около",
+    slovak: "tak (približne)",
+    learned: false,
+    id: "108"
+  },
+  {
+    russian: "не позже пяти (часов)",
+    slovak: "najneskôr o piatej",
+    learned: false,
+    id: "109"
+  },
+  {
+    russian: "несколько",
+    slovak: "zopár",
+    learned: false,
+    id: "110"
+  },
+  {
+    russian: "художница",
+    slovak: "výtvarníčka, umelkyňa",
+    learned: false,
+    id: "111"
   }
 ];
